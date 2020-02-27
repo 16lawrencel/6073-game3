e807afb74e2e65f8f4b3ceeed8b2336a8da827b9
Lawrence Li <lawli@mit.edu>
2020-02-27 15:30:34 -0500

    Player rotates to face toward mouse

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    internal Rotation rotator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotator = GetComponent<Rotation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rotator.FaceToward(mousePosition);
+    }
+}
