be0e791d69f337ec234961fcb7244183e94151d5
Lawrence Li <lawli@mit.edu>
2020-02-27 14:59:58 -0500

    Add movement script for player

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public static KeyCode KEY_UP = KeyCode.W;
+    public static KeyCode KEY_LEFT = KeyCode.A;
+    public static KeyCode KEY_DOWN = KeyCode.S;
+    public static KeyCode KEY_RIGHT = KeyCode.D;
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Globals;
 
 public class Movement : MonoBehaviour
 {
+    internal float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,30 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float movex = 0;
+        float movey = 0;
+
+        if (Input.GetKey(Globals.KEY_UP))
+        {
+            movey += speed;
+        }
+
+        if (Input.GetKey(Globals.KEY_DOWN))
+        {
+            movey -= speed;
+        }
+
+        if (Input.GetKey(Globals.KEY_LEFT))
+        {
+            movex -= speed;
+        }
+
+        if (Input.GetKey(Globals.KEY_RIGHT))
+        {
+            movex += speed;
+        }
+
+        var move = new Vector3(movex, movey, 0);
+        transform.position += move * Time.deltaTime;
     }
 }
