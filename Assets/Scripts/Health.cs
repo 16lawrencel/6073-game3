c8450cc06a2763480e19e886239309c95198fd5b
Lawrence Li <lawli@mit.edu>
2020-02-27 15:10:23 -0500

    Add Healthbar and health prefab / scripts

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHP = 1;
+    internal int currentHP;
+
+    void Awake()
+    {
+        currentHP = maxHP;
+    }
+
+    /// Increment the HP of the entity.
+    public void Increment(int by = 1)
+    {
+        currentHP = Mathf.Clamp(currentHP + by, 0, maxHP);
+    }
+
+    /// Decrement the HP of the entity. Will trigger a HealthIsZero event when
+    /// current HP reaches 0.
+    public void Decrement(int by)
+    {
+        currentHP = Mathf.Clamp(currentHP - by, 0, maxHP);
+    }
+
+    public int GetCurrentHP()
+    {
+        return currentHP;
+    }
+
+    public float Percentage()
+    {
+        return (float)(currentHP) / maxHP;
+    }
+
+    public override string ToString()
+    {
+        return String.Format("{0}/{1}", currentHP, maxHP);
+    }
+}
---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    Health health;
+    internal Transform bar;
+
+    void Awake()
+    {
+        health = GetComponentInParent<Health>();
+    }
+
+    void Start()
+    {
+        bar = transform.Find("Bar");
+        bar.localScale = new Vector2(1f, 1f);
+    }
+
+    void Update()
+    {
+        float percentage = health.Percentage();
+        bool enabled = (percentage != 1f);
+
+        transform.Find("Background").GetComponent<SpriteRenderer>().enabled = enabled;
+        transform.Find("Bar").Find("BarSprite").GetComponent<SpriteRenderer>().enabled = enabled;
+
+        bar.localScale = new Vector2(percentage, 1f);
+    }
+
+    // used by InfoMenu to hook up healthbar to original unit's health
+    public void setHealth(Health newHealth)
+    {
+        health = newHealth;
+    }
+
+}
