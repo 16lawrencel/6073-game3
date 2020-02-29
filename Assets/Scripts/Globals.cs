27cf5d18620a83694999993913d0ecccd3faac55
Elizabeth Zou <ezou@mit.edu>
2020-02-29 15:57:13 -0500

    Add bullet prefab and shooting script

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public static class Globals
 {
-    public static KeyCode KEY_UP = KeyCode.W;
-    public static KeyCode KEY_LEFT = KeyCode.A;
-    public static KeyCode KEY_DOWN = KeyCode.S;
-    public static KeyCode KEY_RIGHT = KeyCode.D;
+    public static KeyCode   KEY_UP = KeyCode.W;
+    public static KeyCode   KEY_LEFT = KeyCode.A;
+    public static KeyCode   KEY_DOWN = KeyCode.S;
+    public static KeyCode   KEY_RIGHT = KeyCode.D;
+    public static float     BULLET_SPEED = 10f;
 }
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bulletPrefab;
+    Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObj = Instantiate(bulletPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            Bullet bullet = (Bullet) bulletObj.GetComponent<Bullet>();
+            bullet.transform.position = gameObject.transform.position;
+            Vector2 mousePosition2d = camera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePosition = new Vector3(mousePosition2d.x, mousePosition2d.y, 0);
+            bullet.setDirection(mousePosition - gameObject.transform.position);
+            bullet.setSpeed(Globals.BULLET_SPEED);
+        }
+    }
+}
