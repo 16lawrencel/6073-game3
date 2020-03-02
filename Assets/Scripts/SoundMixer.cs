a50bc776e0e208aca67418ba8339760c7c495585
ben135wang <61523434+ben135wang@users.noreply.github.com>
2020-03-02 00:41:58 -0500

    Added Scene for Audio work/testing
    Currently there's testing for layering of bgm (switch between layers w/1 and 2)
    
    muting (M)
    
    sound effect (Q)

---
+++ Assets/Scripts/SoundMixer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTesting : MonoBehaviour
+{
+
+    [HideInInspector]
+    public AudioSource bgm;
+
+    public AudioClip track1;
+    public AudioClip track2;
+    public AudioClip test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (bgm.mute) bgm.mute = false;
+            else bgm.mute = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            int playtime = bgm.timeSamples;
+            bgm.clip = track1;
+            bgm.timeSamples = playtime;
+            bgm.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            int playtime = bgm.timeSamples;
+            bgm.clip = track2;
+            bgm.timeSamples = playtime;
+            bgm.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            bgm.PlayOneShot(test);
+        }
+    }
+}
+
