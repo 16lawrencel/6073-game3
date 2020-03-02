d427ad9c67ee1ee9ee456cc19679731c34ab0615
Lawrence Li <lawli@mit.edu>
2020-03-02 16:15:15 -0500

    Restructure scenes so that everything is in a Room object, which gets spawned by a global GameFlow object

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+    protected static GameFlow instance;
+    public static GameFlow Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            instance = FindObjectOfType<GameFlow>();
+            return instance;
+        }
+    }
+
+    public Transform player;
+    public Transform camera;
+    public List<GameObject> rooms;
+
+    public GameObject roomPrefab;
+
+    void Awake()
+    {
+        // QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+        rooms = new List<GameObject>();
+    }
+
+    void Start()
+	{
+        GameObject newRoom = Instantiate(roomPrefab, transform.position, Quaternion.identity);
+        rooms.Add(newRoom);
+	}
+}
