da5715a59f30d6334da5e5faba640df38067226f
Kevin Frans <kevinfrans2@gmail.com>
2020-03-08 23:44:00 -0400

    Made camera more stable, added 3D model test, added on death particles

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
     internal GameObject player;
     internal Health health;
     internal float speed;
+    public Transform onDeath;
     public float THRESHOLD = .01f;
 
     // manually added Start method to be called from subclasses
@@ -25,8 +26,14 @@ public class EnemyAI : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health.Decrement(damage);
-        if (health.GetCurrentHP() <= 0)
-        {
+        if (health.GetCurrentHP() <= 0) {
+            Transform splat = Instantiate(onDeath, 
+                transform.position + new Vector3((Random.value - 0.5f), (Random.value - 0.5f), 0),
+                Quaternion.identity);
+            splat.localScale = new Vector3(1 + Random.value, 1 + Random.value, 0);
+            splat.eulerAngles = new Vector3(0, 0, Random.value * 360);
+            GameObject currentRoom = GameFlow.Instance.GetCurrentRoom();
+            splat.transform.parent = currentRoom.transform.Find("Other");
             Destroy(gameObject);
         }
     }
