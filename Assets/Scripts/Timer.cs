df6a92104e6fb55ae5e9c7cde3999a6facfd84a8
Lawrence Li <lawli@mit.edu>
2020-03-11 00:58:23 -0400

    Add Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float startTime;
+    internal float currentTime;
+
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = Math.Max(0f, currentTime - Time.deltaTime);
+
+        if (currentTime == 0f)
+        {
+            // trigger death scene
+            SceneManager.LoadScene("LoseScreen");
+        }
+
+        timerText.text = "Time left: " + GetTimeString(currentTime);
+        Debug.Log(timerText.text);
+    }
+
+    private string GetTimeString(float time)
+    {
+        string minutes = ((int)(time / 60)).ToString("00");
+        string seconds = ((int)(time % 60)).ToString("00");
+        return minutes + ":" + seconds;
+    }
+}
